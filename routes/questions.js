33d02be5817e090e5bff13aea82c3d24a8bb351c
Steve Godin <steve.god@gmail.com>
2017-04-26 15:35:30 -0700

    Add answer list in Question#show

--- routes/questions.js
+++ routes/questions.js
@@ -29,6 +29,7 @@ router.get('/new', function (req, res) {
   res.render('questions/new', {question: question});
 })
 
+// Questions#create URL: /questions VERB: POST
 router.post('/', function (req, res) {
   // .body is a property of the request object that
   // contains all form data as a JavaScript object
@@ -54,8 +55,19 @@ router.get('/:id', function (req, res) {
   Question
     .findById(id)
     .then(function (question) {
-      res.render('questions/show', {question: question})
-    });
+      return Promise.all([
+        question,
+        question.getAnswers({order: [['createdAt', 'DESC']]})
+      ])
+    })
+    // NEW! Array Destructuring
+    // const [first, second, ...rest] = [1, 2, 3, 4, 5, 6]
+    // first === 1; second === 2, rest === [3, 4, 5. 6]
+    // 👆 can also be done with function arguments 👇
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    .then(function ([question, answers]) {
+      res.render('questions/show', {question: question, answers: answers})
+    })
 })
 
 // URL: /questions/:questionId/answers VERB: All of them!
